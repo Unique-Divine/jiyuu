54e8267551eced4e65746819261345eb7f09ed71
Unique-Divine <realuniquedivine@gmail.com>
2023-08-07 13:28:06 -0500

    (toc.ts): workhorse function

---
+++ src/toc.ts
@@ -0,0 +1,41 @@
+import { Token, generate } from "./generate";
+import { RemarkablePlus } from "./remarkable";
+
+/**
+ * Generates a table of contents (TOC) from markdown headings.
+ *
+ * @param {string} markdown - Markdown string to generate TOC from
+ * @returns {TocResult} TOC result
+ */
+export const toc = (markdown: string, options?: any): TocResult => {
+  return new RemarkablePlus({ options })
+    .use(generate(options))
+    .render(markdown) as unknown as TocResult;
+};
+
+/** Table of contents (TOC) result */
+export interface TocResult {
+  /** Rendered TOC content string */
+  content: string;
+
+  /** Highest heading level found */
+  highest: number;
+
+  /** Array of markdown AST tokens */
+  tokens: Token[];
+
+  /** Array of heading objects */
+  json: Heading[];
+}
+
+/** Heading object */
+interface Heading {
+  /** Heading content text */
+  content: string;
+
+  /** Heading level */
+  lvl: number;
+
+  /** Slugified head ID */
+  slug: string;
+}
