5542c62621d810b634119975c46e9556940f856a
Unique-Divine <realuniquedivine@gmail.com>
2023-08-12 13:31:32 -0500

    top level index.ts for src

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import concat from "concat-stream";
+import mdlink from "markdown-link";
+import minimist from "minimist";
+import merge from "mixin-deep";
+import pick from "object.pick";
+import repeat from "repeat-string";
+import { generate, insert, strip } from "./generate";
+
+export { getTitle, slugify, replaceDiacritics } from "./options";
+
+export { toc } from "./toc";
+export const Toc = {
+  insert,
+  strip,
+  plugin: generate,
+};
