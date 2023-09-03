29d346cca22477f511a2197ee249a8746ec95d7c
Unique-Divine <realuniquedivine@gmail.com>
2023-09-03 13:59:32 -0500

    fix(generate.ts): Fix bug related to incorrect json slugs and tok.seen values

--- src/mdtoc/generate.ts
+++ src/mdtoc/generate.ts
@@ -9,16 +9,16 @@
  * Released under the MIT License.
  */
 
-import { escape as querystringEscape } from "querystring";
-import { RemarkablePlus } from "./remarkable";
-import { slugify } from "./options";
-import markdownLink from "markdown-link";
-import { SlugifyOptions, getTitle } from "./options";
-import listitem from "list-item";
-import { insert } from "./insert";
-export { insert };
+import * as querystring from "querystring" // NOTE querystring API is deprecated
+import markdownLink from "markdown-link"
+import listitem from "list-item"
+import { RemarkablePlus } from "./remarkable"
+import { slugify, SlugifyOptions, getTitle } from "./options"
+import { insert } from "./insert"
 
-export { slugify };
+export { insert }
+
+export { slugify }
 
 interface GenerateOptions extends SlugifyOptions {
   firsth1?: boolean;
@@ -62,90 +62,90 @@ export interface Token {
   lines: number[];
 }
 
-export function generate(options?: GenerateOptions): any {
-  const opts = { ...{ firsth1: true, maxdepth: 6 }, ...options };
-  const stripFirst = opts.firsth1 === false;
-  if (opts.linkify === undefined) opts.linkify = true;
+export interface JsonToken {
+  content: string;
+  slug: string;
+  lvl: number;
+  i: number;
+  seen: number;
+}
 
-  return function (md: RemarkablePlus) {
-    md.renderer.render = function (tokens: Token[]) {
-      const copiedTokens = [...tokens];
-      const seen: { [key: string]: number } = {};
-      let tocstart = -1;
-      const arr: Token[] = [];
+export function generate(options?: GenerateOptions): any {
+  const opts = { ...{ firsth1: true, maxdepth: 6 }, ...options }
+  const stripFirst = opts.firsth1 === false
+  if (opts.linkify === undefined) opts.linkify = true
+
+  return function(md: RemarkablePlus) {
+    md.renderer.render = function(tokens: Token[]) {
+      const copiedTokens = [...tokens]
+      const seen: { [key: string]: number } = {}
+      let tocstart = -1
+      const arr: Token[] = []
       const res: {
-        json?: {
-          content: string;
-          slug: string;
-          lvl: number;
-          i: number;
-          seen: number;
-        }[];
+        json?: JsonToken[];
         highest?: number;
         tokens?: Token[];
         content?: string;
-      } = {};
+      } = {}
 
       for (let i = 0; i < copiedTokens.length; i++) {
-        const token = copiedTokens[i];
+        const token = copiedTokens[i]
         if (/<!--[ \t]*toc[ \t]*-->/.test(token.content)) {
-          tocstart = token.lines[1];
+          tocstart = token.lines[1]
         }
 
         if (token.type === "heading_open") {
-          copiedTokens[i + 1].lvl = copiedTokens[i].hLevel;
-          copiedTokens[i + 1].i = i;
-          arr.push(copiedTokens[i + 1]);
+          copiedTokens[i + 1].lvl = copiedTokens[i].hLevel
+          copiedTokens[i + 1].i = i
+          arr.push(copiedTokens[i + 1])
         }
       }
 
-      let result: Token[] = [];
-      res.json = [];
+      let result: Token[] = []
+      res.json = []
 
       for (const tok of arr) {
         if (tok.lines && tok.lines[0] > tocstart) {
-          let val = tok.content;
+          let val = tok.content
+          if (seen[val] === undefined) seen[val] = 0
           if (tok.children && tok.children[0].type === "link_open") {
             if (tok.children[1].type === "text") {
-              val = tok.children[1].content;
+              val = tok.children[1].content
             }
           }
 
-          if (!seen[val]) {
-            seen[val] = 0;
-          } else {
-            seen[val]++;
-          }
-
-          tok.seen = opts.num = seen[val];
-          tok.slug = slugify(val, opts);
-          res.json.push({
+          tok.seen = seen[val]
+          tok.slug = slugify(val, opts)
+          const jsonToken: JsonToken = {
             content: tok.content,
-            slug: tok.slug,
+            slug: seen[val] === 0 ? tok.slug : `${tok.slug}-${seen[val]}`,
             lvl: tok.lvl,
             i: tok.i,
             seen: tok.seen,
-          });
+          }
+          res.json.push(jsonToken)
 
           if (opts.linkify) {
-            const tokLinkified = linkify(tok, opts);
-            result.push(tokLinkified);
+            const tokLinkified = linkify(tok, opts)
+            result.push(tokLinkified)
           } else {
-            result.push(tok);
+            result.push(tok)
           }
+
+          seen[val] += 1
         }
       }
 
-      opts.highest = highest(result);
-      res.highest = opts.highest;
-      res.tokens = copiedTokens;
+      opts.highest = highest(result)
+      res.highest = opts.highest
+      res.tokens = copiedTokens
 
-      if (stripFirst) result = result.slice(1);
-      res.content = bullets(result, opts);
-      if (opts.append) res.content += opts.append;
-      return res;
-    };
-  };
+      if (stripFirst) result = result.slice(1)
+      res.content = bullets(result, opts)
+      if (opts.append) res.content += opts.append
+      return res
+    }
+  }
 }
 
 /** Func that creates a single markdown list item for the Toc. */
@@ -153,28 +153,28 @@ type ListItemFn = (level: number, content: string) => string;
 
 const isListItemFn = (fn: any): fn is ListItemFn => {
   if (typeof fn !== "function") {
-    return false; // wrong type of object
+    return false // wrong type of object
   } else if (fn.length !== 2) {
-    return false; // wrong number of args
+    return false // wrong number of args
   }
 
   try {
-    const tempResult = fn(1, "test");
-    return typeof tempResult === "string";
+    const tempResult = fn(1, "test")
+    return typeof tempResult === "string"
   } catch (_error) {
     // If calling the func throws an error, it's not a ListItemFn
-    return false;
+    return false
   }
-};
+}
 
 const newListitemFn = (
   opts: GenerateOptions,
   fn: FilterFn | null,
 ): ListItemFn => {
-  const liFn = listitem(opts, fn as Function);
-  if (isListItemFn(liFn)) return liFn;
-  throw Error(`list item fn has the wrong type ${JSON.stringify(liFn)}`);
-};
+  const liFn = listitem(opts, fn as Function)
+  if (isListItemFn(liFn)) return liFn
+  throw Error(`list item fn has the wrong type ${JSON.stringify(liFn)}`)
+}
 
 /**
  * Render markdown list bullets
@@ -187,48 +187,40 @@ export function bullets(arr: Token[], options?: GenerateOptions): string {
   const opts = {
     indent: "  ",
     ...options,
-  };
+  }
 
-  opts.chars = opts.chars || opts.bullets || ["-", "*", "+"];
+  opts.chars = opts.chars || opts.bullets || ["-", "*", "+"]
 
-  const unindent = opts.firsth1 === false ? 1 : 0;
+  const unindent = opts.firsth1 === false ? 1 : 0
 
   // const li =  =>
   //   listitem(opts);
 
-  const fn = typeof opts.filter === "function" ? opts.filter : null;
-  const li: ListItemFn = newListitemFn(opts, fn);
+  const fn = typeof opts.filter === "function" ? opts.filter : null
+  const li: ListItemFn = newListitemFn(opts, fn)
 
-  let res: string[] = [];
+  const res: string[] = []
 
   for (let i = 0; i < arr.length; i++) {
-    const ele = arr[i];
-    ele.lvl -= unindent;
-    console.debug("DEBUG obj: %o", {
-      ele,
-      opts,
-    });
+    const ele = arr[i]
+    ele.lvl -= unindent
 
     if (fn && !fn(ele.content, ele, arr)) {
-      continue;
+      continue
     }
 
-    const maxDepth = opts.maxdepth ?? 3;
+    const maxDepth = opts.maxdepth ?? 3
     if (ele.lvl > maxDepth) {
-      continue;
+      continue
     }
 
-    const lvl = ele.lvl - opts.highest;
-    const out = li(lvl, ele.content);
+    const lvl = ele.lvl - opts.highest
+    const out = li(lvl, ele.content)
 
-    console.debug("DEBUG obj: %o", {
-      out,
-    });
-    res.push(out);
-    // res.push(li(lvl, ele.content, opts));
+    res.push(out)
   }
 
-  return res.join("\n");
+  return res.join("\n")
 }
 
 /**
@@ -239,70 +231,72 @@ export function bullets(arr: Token[], options?: GenerateOptions): string {
  * @return {Number} Highest level
  */
 export function highest(arr: Token[]): number {
-  const sorted = arr.slice().sort((a, b) => a.lvl - b.lvl);
+  const sorted = arr.slice().sort((a, b) => a.lvl - b.lvl)
 
-  return sorted.length ? sorted[0].lvl : 0;
+  return sorted.length ? sorted[0].lvl : 0
 }
 
 /**
  * Turn headings into anchors
  */
 export function linkify(tok: Token, options?: GenerateOptions): Token {
-  const opts = { ...options };
+  const opts = { ...options }
   if (tok && tok.content) {
-    opts.num = tok.seen;
-    const text = titleize(tok.content, opts);
-    const slug = slugify(tok.content, opts);
-    const escapedSlug = querystringEscape(slug);
+    opts.num = tok.seen
+    const text = titleize(tok.content, opts)
+    const slug = slugify(tok.content, opts)
+    // const escapedSlug = querystringEscape(slug)
+    const escapedSlug = (querystring as any).escape(slug)
 
+    // console.debug("DEBUG linkfiy: %o", { escapedSlug, opts, tok, text, slug, })
+    let linkifyFunc: LinkifyFn = (_tok, text, escapedSlug, _opts) => markdownLink(text, `#${escapedSlug}`)
     if (opts && typeof opts.linkify === "function") {
-      return opts.linkify(tok, text, escapedSlug, opts);
+      linkifyFunc = opts.linkify
     }
-
-    tok.content = markdownLink(text, "#" + escapedSlug);
+    tok.content = linkifyFunc(tok, text, escapedSlug, opts)
   }
-  return tok;
+  return tok
 }
 /**
  * Titleize the title part of a markdown link.
  */
 export function titleize(str: string, opts?: GenerateOptions): string {
   if (opts && opts.strip) {
-    return strip(str, opts);
+    return strip(str, opts)
   }
 
-  if (opts && opts.titleize === false) return str;
+  if (opts && opts.titleize === false) return str
 
   if (opts && typeof opts.titleize === "function") {
-    return opts.titleize(str, opts);
+    return opts.titleize(str, opts)
   }
 
-  str = getTitle(str);
+  str = getTitle(str)
 
-  str = str.split(/<\/?[^>]+>/).join("");
+  str = str.split(/<\/?[^>]+>/).join("")
 
-  str = str.split(/[ \t]+/).join(" ");
+  str = str.split(/[ \t]+/).join(" ")
 
-  return str.trim();
+  return str.trim()
 }
 
 /**
  * Optionally strip specified words from heading text (not url)
  */
 export function strip(str: string, opts?: GenerateOptions): string {
-  if (!opts) return str;
-  if (!opts.strip) return str;
+  if (!opts) return str
+  if (!opts.strip) return str
 
   if (typeof opts.strip === "function") {
-    return opts.strip(str, opts);
+    return opts.strip(str, opts)
   }
 
   if (Array.isArray(opts.strip) && opts.strip.length) {
-    const res = opts.strip.join("|");
-    const re = new RegExp(res, "g");
-    str = str.trim().replace(re, "");
-    return str.replace(/^-|-$/g, "");
+    const res = opts.strip.join("|")
+    const re = new RegExp(res, "g")
+    str = str.trim().replace(re, "")
+    return str.replace(/^-|-$/g, "")
   }
 
-  return str;
+  return str
 }
