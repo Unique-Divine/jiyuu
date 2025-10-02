938d50fbdfb2dc4f696cb79a453a217a6a58f546
Unique Divine <realuniquedivine@gmail.com>
2025-10-02 17:32:43 -0500

    bash: publish @uniquedivine/bash v0.0.2 on npm

--- bash-ts/index.ts
+++ bash-ts/index.ts
@@ -1,6 +1,7 @@
 import type { Subprocess } from "bun"
 import * as Bun from "bun"
 
+/** Output result of a BASH command */
 export interface BashOut {
   stdout: string
   stderr: string
@@ -8,7 +9,7 @@ export interface BashOut {
 }
 
 export const bash = async (cmd: string): Promise<BashOut> => {
-  const rawOut: Subprocess = Bun.spawn(["bash", "-c", cmd])
+  const rawOut: Subprocess = Bun.spawn(["bash", "-o", "pipefail", "-c", cmd])
   const { stdout, stderr } = rawOut
   const exitCode = await rawOut.exited
   return {
