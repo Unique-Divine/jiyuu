12bf772d7f645781f1aee5cd5f1e7d053d964a1b
Unique Divine <realuniquedivine@gmail.com>
2025-11-13 15:20:58 -0600

    algos: 001 add two sum problem statement

--- algos/001_two_sum_test.go
+++ algos/001_two_sum_test.go
@@ -11,19 +11,15 @@ package algos
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
+//
 // Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
-
+//
 // Example 3:
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 // ```
-
-// Inputs have exactly one solution -> there's guaranteed to be a correct answer.
-// Cannot use same element twice -> [0, 0] is not a valid answer, even if it
-// evaluates to the target.
 
 func Sol1(nums []int, target int) [2]int {
 	if len(nums) < 2 {
